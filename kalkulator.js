7a901d3c378fb6defb9a67ad4479f7b8110befb1
ibnuaimar <ibnuaimar133@Gmail.ccom>
2025-04-27 23:25:38 +0800

    kedua

---
+++ kalkulator.js
@@ -0,0 +1,22 @@
+function append(char) {
+    const display = document.getElementById('display');
+    display.value += char;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
